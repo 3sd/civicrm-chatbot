62ba0dfafbbf07820e511ad3d47a445813790613
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-03-06 10:42:24 +0000

    nicer arrows :)

--- CRM/Chatbot/Middleware/IncomingLog.php
+++ CRM/Chatbot/Middleware/IncomingLog.php
@@ -6,7 +6,7 @@ use BotMan\BotMan\BotMan;
 class CRM_Chatbot_Middleware_IncomingLog implements Received {
 
   public function received(IncomingMessage $message, $next, BotMan $bot) {
-    CRM_Chatbot_Logger::log("Message <-- contact_id ".$message->getExtras('contact_id') . ':' . $message->getText());
+    CRM_Chatbot_Logger::log("Message <- contact_id ".$message->getExtras('contact_id') . ':' . $message->getText());
     return $next($message);
   }
 
--- CRM/Chatbot/Middleware/OutgoingLog.php
+++ CRM/Chatbot/Middleware/OutgoingLog.php
@@ -8,7 +8,7 @@ class CRM_Chatbot_Middleware_OutgoingLog implements Sending {
 
   public function sending($payload, $next, BotMan $bot)
   {
-    CRM_Chatbot_Logger::log("Message --> contact_id ".$bot->getMessage()->getExtras('contact_id') . ':' . $payload['message']['text']);
+    CRM_Chatbot_Logger::log("Message -> contact_id ".$bot->getMessage()->getExtras('contact_id') . ':' . $payload['message']['text']);
     return $next($payload);
   }
 
