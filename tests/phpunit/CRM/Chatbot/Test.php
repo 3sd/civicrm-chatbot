dc7d3865d0a255dac81d41a8c1da2b579ccc631a
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-03-07 12:38:19 +0000

    feat: Add test for Identify Middleware

--- tests/phpunit/CRM/Chatbot/Test.php
+++ tests/phpunit/CRM/Chatbot/Test.php
@@ -1,33 +1,19 @@
 <?php
-use BotMan\BotMan\BotManFactory;
+
+use Civi\Test\HeadlessInterface;
+use Civi\Test\HookInterface;
+use Civi\Test\TransactionalInterface;
+
 /**
- * This is a generic test class for the extension (implemented with PHPUnit).
+ * @group headless
  */
-class CRM_Chatbot_Test extends CRM_Chatbot_Base {
+abstract class CRM_Chatbot_Test extends \PHPUnit_Framework_TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
 
-  /**
-   * The setup() method is executed before the test is executed (optional).
-   */
-  public function setUp() {
-    parent::setUp();
+  function setUpHeadless() {
+
+    return Civi\Test::headless()
+      ->installMe(__DIR__)
+      ->apply();
   }
 
-  /**
-   * The tearDown() method is executed after the test was executed (optional)
-   * This can be used for cleanup.
-   */
-  public function tearDown() {
-    parent::tearDown();
-  }
-
-  /**
-   * Simple example test case.
-   *
-   * Note how the function name begins with the word "test".
-   */
-  public function testExample() {
-    $b = BotManFactory::create([]);
-    $this->assertEquals(get_class($b), 'BotMan\BotMan\BotMan');
-  }
-  
 }
