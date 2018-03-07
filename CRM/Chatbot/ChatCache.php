0dac14b375e471c7af32bbfec8394f37e94d40b0
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-03-07 12:47:17 +0000

    refactor: chatcache out of bao

---
+++ CRM/Chatbot/ChatCache.php
@@ -0,0 +1,83 @@
+<?php
+use BotMan\BotMan\Interfaces\CacheInterface;
+
+class CRM_Chatbot_ChatCache implements CacheInterface
+  {
+
+    public function has($key){
+      $entry = new CRM_Chatbot_BAO_ChatCache;
+      $entry->identifier = $key;
+      return (bool) $entry->find();
+    }
+
+    /**
+     * Retrieve an item from the cache by key.
+     *
+     * @param  string $key
+     * @param  mixed $default
+     * @return mixed
+     */
+    public function get($key, $default = null){
+      $this->prune();
+      $entry = new CRM_Chatbot_BAO_ChatCache;
+      $entry->identifier = $key;
+      if($entry->find()){
+        $entry->fetch();
+        return unserialize($entry->value);
+      }
+      return $default;
+    }
+
+    /**
+     * Retrieve an item from the cache and delete it.
+     *
+     * @param  string $key
+     * @param  mixed $default
+     * @return mixed
+     */
+    public function pull($key, $default = null){
+      $this->prune();
+      $entry = new CRM_Chatbot_BAO_ChatCache;
+      $entry->identifier = $key;
+      if($entry->find()){
+        $entry->fetch();
+        $value = $entry->value;
+        $entry->delete();
+        return unserialize($value);
+      }
+      return $default;
+
+    }
+
+    /**
+     * Store an item in the cache.
+     *
+     * @param  string $key
+     * @param  mixed $value
+     * @param  \DateTime|int $minutes
+     * @return void
+     */
+    public function put($key, $value, $minutes){
+      if(!(is_integer($minutes) || $minutes < 1)){
+        return;
+      }
+      $entry = new CRM_Chatbot_BAO_ChatCache;
+      $entry->identifier = $key;
+      if($entry->find()){
+        $entry->fetch();
+        $entry->expires = date_format(new DateTime("+{$minutes} minutes"), 'Y-m-d H:i:s');
+        $entry->value = serialize($value);
+        $entry->update();
+      }else{
+        $entry->expires = date_format(new DateTime("+{$minutes} minutes"), 'Y-m-d H:i:s');
+        $entry->value = serialize($value);
+        $entry->insert();
+      }
+    }
+
+    public function prune(){
+      $expired = new CRM_Chatbot_BAO_ChatCache;
+      $expired->whereAdd('expires < NOW()');
+      $expired->delete(DB_DATAOBJECT_WHEREADD_ONLY);
+    }
+}
