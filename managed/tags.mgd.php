a2b473dde347545d9c07a80d47215af8aa7aa823
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-03-19 20:26:37 +0000

    add chatbot tag

---
+++ managed/tags.mgd.php
@@ -0,0 +1,14 @@
+<?php
+return [
+  [
+    'name' => 'Added by chatbot',
+    'entity' => 'Tag',
+    'params' => [
+      'version' => 3,
+      'name' => 'Chatbot',
+      'description' => 'Contacts added by chatbot',
+      'is_reserved' => '1',
+      'is_active' => '1'
+    ]
+  ]
+];
