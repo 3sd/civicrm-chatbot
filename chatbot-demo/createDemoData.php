9c2845ba52a4d0d9308dc8a16161e1ef1e38e11c
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-03-19 23:31:41 +0000

    feat: demo data generator

--- chatbot-demo/createDemoData.php
+++ chatbot-demo/createDemoData.php
@@ -16,12 +16,95 @@ foreach($entities as $entity){
 
 echo "Creating demo data...\n";
 
-$conversation = civicrm_api3('ChatConversationType', 'create', [
-  'name' => 'Cats or dogs?',
-  'timeout' => '30',
-]);
+$entities['ChatConversationType'] = [
+  'pets' => [
+    'name' => 'Cats or dogs?',
+    'timeout' => '30'
+  ],
+  'fullName' => [
+    'name' => 'Full name',
+    'timeout' => '30'
+  ],
+];
 
-$hears = civicrm_api3('ChatHear', 'create', [
-  'text' => 'demo',
-  'chat_conversation_type_id' => $conversation['id'],
-]);
+$entities['ChatHear'] = [
+  'petsurvey' => [
+    'text' => 'pet survey',
+    'chat_conversation_type_id' => '{{ChatConversationType.pets}}'
+  ]
+];
+
+$entities['ChatQuestion'] = [
+  'dogorcat' => [
+    'conversation_type_id' => '{{ChatConversationType.pets}}',
+    'text' => 'Are you a dog person or a cat person?',
+  ],
+  'dog' => [
+    'conversation_type_id' => '{{ChatConversationType.pets}}',
+    'text' => 'Do you have a dog?',
+  ],
+  'cat' => [
+    'conversation_type_id' => '{{ChatConversationType.pets}}',
+    'text' => 'Do you have a cat?',
+  ],
+  'catsName' => [
+    'conversation_type_id' => '{{ChatConversationType.pets}}',
+    'text' => 'What is your cats name?',
+  ],
+  'dogNewsletter' => [
+    'conversation_type_id' => '{{ChatConversationType.pets}}',
+    'text' => 'Would you like to join our newsletter for Dog owners?'
+  ],
+  'firstName' => [
+    'conversation_type_id' => '{{ChatConversationType.fullName}}',
+    'text' => 'What is your first name?',
+  ],
+  'lastName' => [
+    'conversation_type_id' => '{{ChatConversationType.fullName}}',
+    'text' => 'What is your last name?',
+  ],
+];
+
+$created = [];
+while(createEntities($entities, $created));
+var_dump($created);
+var_dump(civicrm_api3('ChatConversationType', 'create', [
+  'id' => $created['ChatConversationType']['pets'],
+  'first_question_id' => $created['ChatQuestion']['dogorcat']
+]));
+
+var_dump(civicrm_api3('ChatConversationType', 'create', [
+  'id' => $created['ChatConversationType']['fullName'],
+  'first_question_id' => $created['ChatQuestion']['firstName']
+]));
+
+
+function createEntities(&$entities, &$created){
+  foreach($entities as $type => $objects){
+    foreach($objects as $tag => $params){
+      echo "$type.$tag\n";
+      if(substitute($params, $created)){
+        $result = civicrm_api3($type, 'create', $params);
+        $created[$type][$tag] = $result['id'];
+        unset($entities[$type][$tag]);
+      }
+    }
+    if(count($entities[$type] == 0)){
+      unset($entities[$type]);
+    }
+  }
+  return count($entities);
+}
+
+function substitute(&$params, $created){
+  foreach($params as $key => $value){
+    if(preg_match("/^{{(.*)\.(.*)}}$/", $value, $matches)){
+      if(isset($created[$matches[1]][$matches[2]])){
+        $params[$key] = $created[$matches[1]][$matches[2]];
+      }else{
+        return false;
+      }
+    }
+  }
+  return true;
+}
