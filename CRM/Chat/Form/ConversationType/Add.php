da82b3d0bdcc29e010005f615f9541fc52a2edb1
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-04-19 15:52:43 +0100

    feature: form to add conversation types

--- CRM/Chat/Form/ConversationType/Add.php
+++ CRM/Chat/Form/ConversationType/Add.php
@@ -7,99 +7,76 @@ use CRM_Chat_ExtensionUtil as E;
  *
  * @see https://wiki.civicrm.org/confluence/display/CRMDOC/QuickForm+Reference
  */
-class CRM_Chat_Form_ConversationType_Add extends CRM_Core_Form {
 
-  function buildQuickForm() {
+class CRM_Chat_Form_ConversationType_Add extends CRM_Chat_Form_Good {
 
-    $this->formFields = [
-      [
-        'name' => 'ChatConversationType.name',
-        'html_type' => 'text',
-        'title' => 'Name',
-        'help_text' => 'A descriptive name for the conversation',
-        'required' => true,
-      ],
-      [
-        'name' => 'ChatQuestion.text',
-        'html_type' => 'text',
-        'title' => 'First question',
-        'help_text' => 'The first question that will be asked when this conversation starts',
-        'required' => true,
-      ],
-      [
-        'name' => 'ChatConversationType.timeout',
-        'html_type' => 'text',
-        'title' => 'Timeout',
-        'help_text' => 'The amount of time in minutes a recipient has to reply to the convesation.',
-        'default' => '30',
-        'required' => true,
+  var $title = 'Add Conversation type';
+
+  var $fields = [
+    [
+      'entity' => 'ChatConversationType',
+      'field' => 'name',
+      'title' => 'Conversation name',
+      'help' => 'A descriptive name for the conversation',
+    ],
+    [
+      'entity' => 'ChatQuestion',
+      'field' => 'text',
+      'title' => 'Opening question',
+      'help' => 'The first question of this conversation',
+    ],
+    [
+      'entity' => 'ChatConversationType',
+      'field' => 'timeout',
+      'title' => 'Timeout',
+      'help' => 'Time in minutes, after which this conversation should be considered complete',
+    ],
+  ];
+
+  var $entities = [
+    'ChatConversationType' => [
+      'type' => 'ChatConversationType',
+    ],
+    'ChatQuestion' => [
+      'type' => 'ChatQuestion',
+      'references' => [
+        'conversation_type_id' => [
+          'entity' => 'ChatConversationType',
+          'field' => 'id'
+          ]
       ]
-    ];
-    $this->addFields();
-    $this->addButtons([
-      array('type' => 'cancel', 'name' => 'Cancel'),
-      array('type' => 'submit', 'name' => 'Add', 'isDefault' => TRUE)
-    ]);
-    $this->assign('formGroups', $this->getFormGroups());
-    $this->assign('help', $this->help);
-    parent::buildQuickForm();
+    ]
+  ];
+
+  var $submitText = 'Add';
+
+  function setDefaultValues(){
+
+    $defaults['ChatConversationType:timeout'] = 30;
+    return $defaults;
 
   }
 
-  function addFields(){
-    foreach($this->formFields as $field){
-      $this->addFormField($field);
-      if(isset($field['help_text'])){
-        $this->addHelp('field', $field['name'], $field['help_text']);
-      }
-    }
-
+  function getDestination() {
+    return CRM_Utils_System::url('civicrm/chat/conversationType/view', 'id='.$this->entities['ChatConversationType']['after']['id']);
   }
 
-  function addFormField($element) {
-    switch ($element['html_type']) {
-        case 'text':
-          $element = $this->addElement(
-            'text',
-            $element['name'],
-            ts($element['title']),
-            1
-          );
-          if(isset($element->_attributes['class'])){
-            $element->_attributes['class'] .= ' form-control';
-          }else{
-            $element->_attributes['class'] = 'form-control';
-          }
-
-          break;
-
-        default:
-          # code...
-          break;
-    }
+  function getContext() {
+    return CRM_Utils_System::url('civicrm/chat/conversationType');
   }
 
-  function addHelp($type, $key, $text){
-    $this->help[$type][$key] = $text;
-  }
-
-  function getFormGroups(){
-    $formGroups = [];
-    foreach ($this->_elements as $element) {
-      $label = $element->getLabel();
-      if (!empty($label)) {
-        $formGroups[] = $element->getName();
-      }
-    }
-    return $formGroups;
-  }
 
   function postProcess(){
 
-    $submitted = $this->exportValues();
-    var_dump($submitted);
-    exit;
-    // Set destination to newly created conversation
+    parent::postProcess();
+
+    $params = [
+      'id' => $this->entities['ChatConversationType']['after']['id'],
+      'first_question_id' => $this->entities['ChatQuestion']['after']['id']
+    ];
+
+    $result = civicrm_api3('ChatConversationType', 'create', $params);
+
   }
 
 }
