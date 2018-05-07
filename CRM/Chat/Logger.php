fc9491e302d51e229dde6dfbb2639fd5046d084c
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-05-07 21:33:51 +0100

    feature: add file and line to debug logs

--- CRM/Chat/Logger.php
+++ CRM/Chat/Logger.php
@@ -7,7 +7,12 @@ class CRM_Chat_Logger {
     if(!is_string($message)){
       $message = var_export($message, true);
     }
-    foreach(explode("\n", $message) as $line){
+
+    $backtrace = debug_backtrace();
+    if (!empty($backtrace[0]) && is_array($backtrace[0])) {
+      $fileinfo = $backtrace[0]['file'] . ":" . $backtrace[0]['line'];
+    }
+    foreach(array_merge([$fileinfo], explode("\n", $message)) as $line){
       file_put_contents(CRM_Core_Config::singleton()->configAndLogDir . '/chatbot.log', $line . "\n", FILE_APPEND);
       error_log($line);
     }
